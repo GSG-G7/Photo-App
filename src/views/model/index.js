a4a3ba682794e473abb3bf29c5ddec6962c25f79
Mohammed-Q96 <mhmmade@gmail.com>
2019-08-06 13:54:37 +0300

    inital  commit Relates #1

---
+++ src/views/model/index.js
@@ -0,0 +1,7 @@
+module.exports = [
+  { name: 'Noel', yearOfBirth: 1989, admin: true, loggedIn: true },
+  { name: 'Farima', yearOfBirth: 1993, admin: false, loggedIn: true },
+  { name: 'Laia', yearOfBirth: 1978, admin: false, loggedIn: false },
+  { name: 'Sophia', yearOfBirth: 2001, admin: false, loggedIn: false },
+  { name: 'Nareh', yearOfBirth: 2000, admin: true, loggedIn: false },
+];
---
+++ test/test.test.js
@@ -0,0 +1,9 @@
+var test = require('tape');
+
+test('Phto-app', function(t) {
+  t.plan(1);
+  t.test('inital test', t => {
+    t.equal(1, 1);
+    t.end()
+  });
+});
