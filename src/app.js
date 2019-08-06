6416be489e85ef6c2e1eaad74bbe0c49bb7f2284
Mohammed-Q96 <mhmmade@gmail.com>
2019-08-06 15:47:06 +0300

    Created the hbs files and engine relates #5

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
 const controllers = require('./views/controllers');
 const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
 const app = express();
 
-app.set('port', process.env.PORT || 2010);
-app.set('hostname', process.env.HOSTNAME || 'localhost');
+app.set('views', path.join(__dirname, 'views')); // setting the views dir
+app.set('view engine', 'hbs'); // file extintion for engine
+
+app.engine(
+  'hbs',
+  exphbs({
+    extname: 'hbs',
+    layoutsDir: path.join(__dirname, 'views', 'layout'),
+    defaultLayout: 'main'
+  })
+);
 
 app.use(controllers);
+app.set('port', process.env.PORT || 2010);
+app.set('hostname', process.env.HOSTNAME || 'localhost');
 
 module.exports = app;
