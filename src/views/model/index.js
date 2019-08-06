953b02e5e7f64abc4282af0fcfa68a2b2f20ca93
Mohammed-Q96 <mhmmade@gmail.com>
2019-08-06 13:57:44 +0300

    fix bug

--- src/views/model/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-  { name: 'Noel', yearOfBirth: 1989, admin: true, loggedIn: true },
-  { name: 'Farima', yearOfBirth: 1993, admin: false, loggedIn: true },
-  { name: 'Laia', yearOfBirth: 1978, admin: false, loggedIn: false },
-  { name: 'Sophia', yearOfBirth: 2001, admin: false, loggedIn: false },
-  { name: 'Nareh', yearOfBirth: 2000, admin: true, loggedIn: false },
-];
