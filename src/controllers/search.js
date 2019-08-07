a2b66c528bdbd9fbef49c2865ff089eebb871bcc
Mohammed-Q96 <mhmmade@gmail.com>
2019-08-07 11:22:17 +0300

    final commit

---
+++ src/controllers/search.js
@@ -0,0 +1,20 @@
+const nodeFetch = require('node-fetch');
+require('dotenv/config');
+
+const API = {
+  url: 'https://api.unsplash.com/search/photos',
+  key: process.env.API_KEY,
+  getUrl(keyword) {
+    return `${this.url}?query=${keyword}&page=1&per_page=5&client_id=${
+      this.key
+    }`;
+  }
+};
+
+const search = (req, res) => {
+  nodeFetch(API.getUrl(req.body.search))
+    .then(_res => _res.json())
+    .then(data => res.send(data));
+};
+
+module.exports = search;
