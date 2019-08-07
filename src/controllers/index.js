b90bf356f7f3655a2dc246daacd33f73314c0608
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-08-07 11:34:31 +0300

    handled client side errors and server side errors

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const home = require('./home');
+const { client, server } = require('./error');
 
 const router = express.Router();
 
 router.get('/', home);
+router.use(client);
+router.use(server);
 
 module.exports = router;
