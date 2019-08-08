34aa6f7be307084f92f926da60072bf9a37778a0
Mohammed-Q96 <mhmmade@gmail.com>
2019-08-08 10:06:14 +0300

    refactor

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,6 +1,7 @@
-const input = document.querySelector('.main__input');
-const form = document.querySelector('.main__form');
-const span = document.querySelector('.main__span');
+const selector = query => document.querySelector(query);
+const input = selector('.main__input');
+const form = selector('.main__form');
+const span = selector('.main__span');
 form.addEventListener('input', e => {
   if (!input.value) {
     span.textContent = 'This Filed Is Required Honey';
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-app.listen(app.get('port'), () => {
+app.listen(app.get('port'), app.get('hostname'), () => {
   // eslint-disable-next-line no-console
   console.log(
     `The server is running on http://${app.get('hostname')}:${app.get('port')}`
